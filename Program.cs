aa1375334279fb8d1fd31a308f85986216efa6cd
John <thecoxcableguy@gmail.com>
2017-06-12 08:22:15 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Blackjack
+{
+    class Overloading
+    {
+
+
+    }
+    // suit enum
+    // card number enums
+    // loop over and assign values
+    //randomize card deck
+
+    class Generics
+    {
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start game, deal
+
+            //player hit or stay
+
+            //hit? bust? lose
+
+            //computer plays if player is <= 21
+
+            //declare winner
+
+            //deal next hand
+
+            Console.Write("Enter number: ");
+            var input = Console.ReadLine();
+            var num = Convert.ToInt32(input);
+            Console.WriteLine($"echo: {num}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
